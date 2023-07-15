8cd0b14c09d6d02361bb5ffd6dc968dd6c6708b4
Kadir Çabuk <109894656+CabukKadir10@users.noreply.github.com>
2023-07-15 23:01:46 +0300

    Create ödev4.java

---
+++ ödev4.java
@@ -0,0 +1,13 @@
+package java_odevleri;
+
+public class ödev4 {
+
+	public static void main(String[] args) {
+		int i, sayi=0;
+		
+		for(i=1;i<=10;i++) {
+			sayi +=i;
+			System.out.println(sayi);
+		}
+	}
+}
